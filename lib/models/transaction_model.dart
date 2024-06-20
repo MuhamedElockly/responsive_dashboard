3cba611c7c503db1a67a4e60da601bb6652e03a4
dell <elocklymuhamed@gmail.com>
2024-06-20 20:47:32 +0200

    create transaction model

---
+++ lib/models/transaction_model.dart
@@ -0,0 +1,6 @@
+class TransactionModel {
+  final String tittle, date, amount;
+  final bool isWithdraw;
+
+  TransactionModel({required this.tittle, required this.date, required this.amount, required this.isWithdraw});
+}
