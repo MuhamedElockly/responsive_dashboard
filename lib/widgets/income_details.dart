2496644b021b9ad483e9111ad984a0c9a08cbf23
dell <elocklymuhamed@gmail.com>
2024-06-21 17:27:00 +0200

    create income details

---
+++ lib/widgets/income_details.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class IncomeDetails extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class IncomeItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
