4048e5c0b99de3f1341cdea7dd08ef6a160b9c9d
dell <elocklymuhamed@gmail.com>
2024-07-02 13:59:53 +0200

    add drawer

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,20 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dashboard/widgets/adaptive_layout.dart';
+import 'package:responsive_dashboard/widgets/custom_drawer.dart';
 import 'package:responsive_dashboard/widgets/desktop_layout.dart';
 import 'package:responsive_dashboard/widgets/mobile_layout.dart';
 import 'package:responsive_dashboard/widgets/tablet_layout.dart';
 
-class DashBoard extends StatelessWidget {
+class DashBoard extends StatefulWidget {
+  @override
+  State<DashBoard> createState() => _DashBoardState();
+}
+
+class _DashBoardState extends State<DashBoard> {
+  final GlobalKey<ScaffoldState> scafKey = GlobalKey();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scafKey,
       appBar: MediaQuery.of(context).size.width < 800
           ? AppBar(
-              leading: GestureDetector(onTap: () {}, child: Icon(Icons.menu)),
+              leading: GestureDetector(
+                  onTap: () {
+                    scafKey.currentState!.openDrawer();
+                  },
+                  child: Icon(Icons.menu)),
               backgroundColor: Color(0xffFAFAFA),
             )
           : null,
       backgroundColor: Color(0xFFF7F9F8),
+      drawer: MediaQuery.of(context).size.width < 800 ? CustomDrawer() : null,
       body: AdaptiveLayout(
         mobileLayout: (context) => MobileLayout(),
         tabletLayout: (context) => TabletLayout(),
